3c3ffb34268ef25223d712892cd25905b23cd2d3
xuki <574325507@qq.com>
2020-08-31 12:21:07 +0800

    feat:修改签到

--- Xuki/Ifttt/__init__.py
+++
@@ -1,4 +0,0 @@
-# @Time : 2020/8/28 17:00
-# @Author : Xuki
-# @File : __init__.py
-# @Annotation :
--- Xuki/config.py
+++
@@ -1,7 +0,0 @@
-# @Time : 2020/8/28 18:06
-# @Author : Xuki
-# @File : config.py
-# @Annotation :
-
-# 签到
-CHECK = True
